103b46417a36b48eed94b2125aa96f26922aa401
David Williamson <williamsondavidv@outlook.com>
2024-10-08 08:24:09 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Deck from "./Deck.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Deck />
+    </div>
+  );
+}
+
+export default App;
